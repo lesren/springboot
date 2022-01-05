6731b83dbecab35c95269bb6a6e8216bcf1335c0
lsh <lsh>
2022-01-05 14:26:35 +0900

    [spring]
    details :
    1. 페이징 방법 3가지 실습
    2. 데이터테이블

--- ksmart41_mybatis/src/main/java/ksmart41/mybatis/service/MemberService.java
+++ ksmart41_mybatis/src/main/java/ksmart41/mybatis/service/MemberService.java
@@ -38,11 +38,94 @@ public class MemberService {
 	*/
 	
 	//생성자메소드 주입방식
-	private MemberMapper memberMapper;
+	private final MemberMapper memberMapper;
 	
 	public MemberService(MemberMapper memberMapper) {
 		this.memberMapper = memberMapper;
 	}
+	
+	public List<Map<String, Object>> getLoginHistory3() {
+		return memberMapper.getLoginHistoryList(null);
+	}
+	
+	public Map<String, Object> getLoginHistory2(int currentPage) {
+		
+		// 보여줄 행의 갯수
+		int rowPerPage = 5;
+		
+		// 로그인 이력 테이블 행의 갯수
+		double rowCount = memberMapper.getLoginHistoryCount();
+		
+		// 마지막 페이지
+		int lastPage = (int) Math.ceil((rowCount  / rowPerPage));
+		
+		// 페이지 구하기
+		int startNum = 0;
+		startNum = (currentPage - 1) * rowPerPage;
+		
+		// 보여줄 시작페이지 번호
+		int startPageNum = 1;
+		
+		// 보여줄 마지막페이지 번호
+		int endPageNum = 10;
+		
+		// 동적 페이지구성( 7페이지 부터 )
+		if(currentPage >= 7) {
+			startPageNum = currentPage - 5;
+			endPageNum = currentPage + 4;
+			if(endPageNum >= lastPage) {
+				startPageNum = lastPage - 9;
+				endPageNum = lastPage;
+			}
+		}
+		
+		Map<String, Object> paramMap = new HashMap<String, Object>();
+		paramMap.put("startNum", startNum);
+		paramMap.put("rowPerPage", rowPerPage);
+		
+		List<Map <String, Object>> loginHistoryList = memberMapper.getLoginHistoryList(paramMap);
+		
+		paramMap.clear();
+		
+		paramMap.put("lastPage", lastPage);
+		paramMap.put("loginHistoryList", loginHistoryList);
+		paramMap.put("startPageNum", startPageNum);
+		paramMap.put("endPageNum", endPageNum);
+		
+		return paramMap;
+		
+	}
+	public Map<String, Object> getLoginHistory1(int currentPage) {
+		
+		// 보여줄 행의 갯수
+		int rowPerPage = 10;
+		
+		// 로그인 이력 테이블 행의 갯수
+		double rowCount = memberMapper.getLoginHistoryCount();
+		
+		// 마지막 페이지
+		int lastPage = (int) Math.ceil((rowCount  / rowPerPage));
+		
+		// 페이지 구하기
+		int startNum = 0;
+		startNum = (currentPage - 1) * rowPerPage;
+		
+		
+		Map<String, Object> paramMap = new HashMap<String, Object>();
+		paramMap.put("startNum", startNum);
+		paramMap.put("rowPerPage", rowPerPage);
+		
+		List<Map <String, Object>> loginHistoryList = memberMapper.getLoginHistoryList(paramMap);
+		
+		paramMap.clear();
+		paramMap.put("lastPage", lastPage);
+		paramMap.put("loginHistoryList", loginHistoryList);
+		
+		return paramMap;
+		
+	}
+	
+	
 	
 	//회원정보 수정
 	public int modifyMember(Member member) {
